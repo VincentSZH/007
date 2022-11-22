3b83c786661df68eb14fc9ee626d69a5a87d57df
7 <7@qq.com>
2022-11-22 17:11:28 +0800

    继承关卡

--- sesse.sol
+++ sesse.sol
@@ -10,5 +10,15 @@ contract Yeye {
     function yeye() public virtual {
         emit Log("Yeye");
     }
- 
+}
+contract Baba is Yeye{
+    function hip() public virtual override {
+        emit Log("Baba");
+    }
+    function pop() public virtual override {
+        emit Log("Baba");
+    }
+    function baba() public virtual {
+        emit Log("Baba");
+    }
 }
