50060bbd056d4d40dfc6b6484dbf3b3a85a61f6d
7 <7@qq.com>
2022-11-22 20:37:42 +0800

    重载函数实参数传入

--- Overloading.sol
+++ Overloading.sol
@@ -6,5 +6,11 @@ contract overLoading {
     function something(string memory something) public pure returns( string memory ) {
         return(something);
     }
+    function f(uint8 _in) public pure returns(uint out) {
+        out = _in;
+    }
+    function f(uint256 _in) public pure returns(uint256 out) {
+        out = _in;
+    }
 
 }
