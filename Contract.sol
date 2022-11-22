bf307d5dadb7e5a02e492abf5c365f10e7b45ae9
7 <7@qq.com>
2022-11-22 20:00:04 +0800

    接口示例2

--- Contract.sol
+++ Contract.sol
@@ -1,8 +1,17 @@
 pragma solidity ^0.8.4;
-interfact Base{
+interface Name {
     function getFirstName() external pure returns(string memory );
     function getLastName() external pure returns(string memory );
 }
+contract Fullname is Name {
+    function getFirstName() external pure override returns(string memory ) {
+        return("Li");
+    }
+    function getLastName() external pure override returns(string memory ) {
+        return("Jiang");
+    }
+}
+
 abstract contract Base {
     string public name = "Base";
     function getAlias() public pure virtual returns(string memory);
