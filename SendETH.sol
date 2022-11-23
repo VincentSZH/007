2ab47bc1ecedb340979db552d937f70d2b0369a0
7 <7@qq.com>
2022-11-23 15:49:48 +0800

    发送以太坊

--- SendETH.sol
+++ SendETH.sol
@@ -1,7 +1,13 @@
 pragma solidity ^0.8.4;
+error SendFailed();
+error CallFailed();
 contract ReceiveETH {
     event Log(uint amount, uint gas);
     receive() external payable {
         emit Log(msg.value, gasleft());
+    }
+    function getBanlance() view public returns(uint) {
+        return address(this).balance;
+
     }
 }
