6935948bbabeadc034bae4ab2a16755b1e47fcab
7 <7@qq.com>
2022-11-23 14:00:30 +0800

    接收

--- Receive.sol
+++ Receive.sol
@@ -1,7 +1,11 @@
 pragma solidity ^0.8.4;
 contract Receive {
     event Received(address Sender , uint Value );
+    event fallbackCalled(address Sender, uint Value, bytes data);
     receive() external payable {
         emit Received(msg.sender, msg.value );
+    }
+    fallback() external payable {
+        emit fallbackCalled(msg.sender, msg.value, msg.data);
     }
 }
