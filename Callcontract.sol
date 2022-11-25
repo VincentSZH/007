d754b2976bec0bad92638d9fe511fd8ac343bd86
7 <7@qq.com>
2022-11-25 19:52:41 +0800

    Call的使用方法

--- Callcontract.sol
+++ Callcontract.sol
@@ -19,8 +19,11 @@ contract Othercontract{
 }
 contract Call {
     event Response( bool success , bytes data );
-    function callSetX(address payable _address, uint256 x) public payable {
-        (bool success , bytes memory data) = _address.call{value : msg.value} (abi.encodeWithSignature("setX(uint256)",x));
-        emit Response(success, data );
-    }
+    function callSetX(address payable _addr, uint256 x) public payable {
+    (bool success, bytes memory data) = _addr.call{value: msg.value}(
+        abi.encodeWithSignature("setX(uint256)", x)
+    );
+
+    emit Response(success, data); 
+}
 }
