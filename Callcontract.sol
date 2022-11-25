8e03f2fb141a77f41f3cefd8570b0dd4368a96fd
7 <7@qq.com>
2022-11-25 19:25:02 +0800

    Call的使用方法

--- Callcontract.sol
+++ Callcontract.sol
@@ -3,8 +3,17 @@ contract Othercontract{
     uint256 private _x = 0;
     event Log(uint amount, uint gas);
     fallback() external payable {}
+
     function getBalance() public view returns(uint) {
         return address(this).balance;
     }
-    
+    function setX(uint256 x) external payable {
+        _x = x;
+        if(msg.value > 0){
+            emit Log(msg.value, gasleft());
+        }
+    }
+    function getX() external view returns(uint x) {
+        x = _x;
+    }
 }
