8f251716c91faafdcdd945430d4bbfe6a198e326
7 <7@qq.com>
2022-11-26 20:40:39 +0800

    合约中创建合约1

---
+++ Creatcontract.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.4;
+contract Pair {
+    address public factory;
+    address public token1;
+    address public token2;
+    constructor() payable {
+        factory = msg.sender;
+    }
+    function initialize(address _token1, address _token2) external {
+        require(msg.sender == factory, "UniswapV2 : FORBIDDEN");
+        token1 = _token1;
+        token2 = _token2;
+    }
+}
