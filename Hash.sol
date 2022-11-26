b51f722112b06200552c578c5e7d0a512a744190
7 <7@qq.com>
2022-11-26 19:26:02 +0800

    哈希3

--- Hash.sol
+++ Hash.sol
@@ -13,4 +13,7 @@ contract Hash {
     function week(string memory _string) public view returns(bool) {
         return keccak256(abi.encodePacked(_string)) == _msg;
     }
+    function strong(string memory string1, string memory string2) public pure returns(bool) {
+        return keccak256(abi.encodePacked(string1)) == keccak256(abi.encodePacked(string2));
+    }
 }
