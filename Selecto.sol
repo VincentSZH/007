5bafd84f2c39d3256cc4b2f80aed221bc36f300d
7 <7@qq.com>
2022-11-26 15:44:20 +0800

    选择器3

--- Selecto.sol
+++ Selecto.sol
@@ -7,4 +7,8 @@ contract selector {
     function mintSelector () external pure returns (bytes4 mSelector) {
         return bytes4(keccak256("mint(address)"));
     }
+    function callWithSinature() external returns (bool, bytes memory) {
+        (bool success, bytes memory data ) = address(this).call(abi.encodeWithSelector(0x6a627842,"0x2c44b726ADF1963cA47Af88B284C06f30380fC78"));
+        return(success,data);
+    }
 }
