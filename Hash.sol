adc7fd9fff0ec8210f4e1b5fb6fa3b6fd99fc016
7 <7@qq.com>
2022-11-26 19:21:34 +0800

    哈希2

--- Hash.sol
+++ Hash.sol
@@ -1,4 +1,16 @@
 pragma solidity ^0.8.4;
 contract Hash {
+    bytes32 _msg = keccak256(abi.encodePacked("0xAA"));
+    function hash(
+        uint _num,
+        address _addr,
+        string memory _string
+    )
+    public pure returns(bytes32) {
+        return keccak256(abi.encodePacked(_num, _string, _addr));
+        }
     
+    function week(string memory _string) public view returns(bool) {
+        return keccak256(abi.encodePacked(_string)) == _msg;
+    }
 }
