6b98b1279afa6d6b6552e16351dc508b188c4f81
7 <7@qq.com>
2022-11-26 15:34:42 +0800

    选择器2

--- Selecto.sol
+++ Selecto.sol
@@ -4,4 +4,7 @@ contract selector {
     function mint(address to ) external {
         emit Log(msg.data);
     }
+    function mintSelector () external pure returns (bytes4 mSelector) {
+        return bytes4(keccak256("mint(address)"));
+    }
 }
