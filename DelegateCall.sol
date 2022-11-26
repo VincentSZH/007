84d3381f773b6daa29d27b9c3150eb51b41b03bd
7 <7@qq.com>
2022-11-26 13:58:02 +0800

    delegateCall 实操3

--- DelegateCall.sol
+++ DelegateCall.sol
@@ -1,8 +1,11 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
 contract C {
     uint public num;
     address public sender;
-    function setV(uint _num) public payable {
+
+    function setVars(uint _num) public payable {
         num = _num;
         sender = msg.sender;
     }
@@ -11,13 +14,14 @@ contract C {
 contract B {
     uint public num;
     address public sender;
-    function callSetVars(address _addr, uint _num) external payable {
-        (bool success , bytes memory data ) = _addr.call(
-            abi.encodeWithSignature("setVar(uint256)", _num)
+
+    function callSetVars(address _addr, uint _num) external payable{
+        (bool success, bytes memory data) = _addr.call(
+            abi.encodeWithSignature("setVars(uint256)", _num)
         );
     }
-    function delegateCallSetVars(address _addr, uint _num) external payable {
-        (bool success , bytes memory data ) = _addr.delegatecall(
+    function delegatecallSetVars(address _addr, uint _num) external payable{
+        (bool success, bytes memory data) = _addr.delegatecall(
             abi.encodeWithSignature("setVars(uint256)", _num)
         );
     }
