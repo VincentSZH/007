49b4b59c10d13a4c1ef45b13a98fc133ff32a96c
7 <7@qq.com>
2022-11-29 16:33:31 +0800

    合约中创建合约5

--- Creatcontract.sol
+++ Creatcontract.sol
@@ -1,24 +1,25 @@
 pragma solidity ^0.8.4;
 contract Pair {
     address public factory;
+    address public token0;
     address public token1;
-    address public token2;
     constructor() payable {
         factory = msg.sender;
     }
-    function initialize(address _token1, address _token2) external {
+    function initialize(address _token0, address _token1) external {
         require(msg.sender == factory, "UniswapV2 : FORBIDDEN");
+        token0 = _token0;
         token1 = _token1;
-        token2 = _token2;
     }
 }
 contract PairFactory {
-    mapping(address => mapping(address => address)) public getPair;
+    mapping(address => mapping(address => address)) public getPair; 
     address[] public allPairs;
+
     function createPair(address tokenA, address tokenB) external returns(address pairAddr) {
         Pair pair = new Pair();
         pair.initialize(tokenA,tokenB);
-        pairAddr = address(paira);
+        pairAddr = address(pair);
         allPairs.push(pairAddr);
         getPair[tokenA] [tokenB] = pairAddr;
         getPair[tokenB] [tokenA] = pairAddr;
