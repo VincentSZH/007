12da1e9a5cb849c515b463ff50f0b33c9e1b342b
7 <7@qq.com>
2022-12-02 13:46:56 +0800

    ABI-1

---
+++ ABI.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.4;
+contract ABI {
+    uint x = 10;
+    address addr = 0x7A58c0Be72BE218B41C608b7Fe7C5bB630736C71;
+    string name = "0xAA";
+    uint[2] array = [5,6];
+    
+
+}
