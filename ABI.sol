bc7ba9ed7c179029a3baa0026c2f08fee89e3118
7 <7@qq.com>
2022-12-02 13:50:33 +0800

    ABI-2

--- ABI.sol
+++ ABI.sol
@@ -4,6 +4,7 @@ contract ABI {
     address addr = 0x7A58c0Be72BE218B41C608b7Fe7C5bB630736C71;
     string name = "0xAA";
     uint[2] array = [5,6];
-    
-
+    function encode() public view returns(bytes memory result) {
+        result = abi.encode(x, addr, name, array);
+    }
 }
