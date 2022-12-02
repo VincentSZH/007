f350361aa5ec8441c604c6dad7ab59819bb4e547
7 <7@qq.com>
2022-12-02 20:11:33 +0800

    catch-1

---
+++ TryCatch.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.4;
+contract OnlyEven{
+    constructor(uint a){
+        require(a != 0, "invalid number");
+        assert(a != 1);
+    }
+
+    function onlyEven(uint256 b) external pure returns(bool success){
+        // 输入奇数时revert
+        require(b % 2 == 0, "Ups! Reverting");
+        success = true;
+    }
+}
